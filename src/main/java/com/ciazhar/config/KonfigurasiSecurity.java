3ef03aff22a76934456d387740ae5e576fd4a16b
ciazhar <daisetsunakama@gmail.com>
2017-03-30 20:58:26 +0700

    setting security

--- src/main/java/com/ciazhar/config/KonfigurasiSecurity.java
+++ src/main/java/com/ciazhar/config/KonfigurasiSecurity.java
@@ -1,7 +1,44 @@
 package com.ciazhar.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 /**
  * Created by ciazhar on 3/30/17.
  */
-public class KonfigurasiSecurity {
+
+@Configuration
+@EnableWebSecurity
+public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter{
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        //setting security non database
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin")
+                .password("123")
+                .roles("OPERATOR");
+    }
+
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**","/js/**").permitAll()
+                .antMatchers("/aplikasi/**").hasAnyRole("OPERATOR")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .and();
+    }
+
 }
--- src/main/java/com/ciazhar/config/KonfigurasiWeb.java
+++ src/main/java/com/ciazhar/config/KonfigurasiWeb.java
@@ -1,7 +1,18 @@
 package com.ciazhar.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 /**
  * Created by ciazhar on 3/30/17.
  */
-public class KonfigurasiWeb {
+
+@Configuration
+public class KonfigurasiWeb extends WebMvcConfigurerAdapter{
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/aplikasi/list").setViewName("aplikasi/list");
+    }
 }
