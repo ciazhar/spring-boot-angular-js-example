775100b7c6813c8c77c7d4343e91973f62ecf98a
ciazhar <daisetsunakama@gmail.com>
2017-03-30 20:55:51 +0700

    fix request mapping

---
+++ src/main/java/com/ciazhar/config/KonfigurasiWeb.java
@@ -0,0 +1,7 @@
+package com.ciazhar.config;
+
+/**
+ * Created by ciazhar on 3/30/17.
+ */
+public class KonfigurasiWeb {
+}
--- src/main/java/com/ciazhar/controllers/AplikasiController.java
+++ src/main/java/com/ciazhar/controllers/AplikasiController.java
@@ -16,6 +16,7 @@ import javax.validation.Valid;
  */
 
 @Controller
+@RequestMapping("api")
 public class AplikasiController {
 
     @Autowired
