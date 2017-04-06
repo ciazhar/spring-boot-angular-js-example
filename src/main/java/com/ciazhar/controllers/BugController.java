899be4a716478ffc2d117ebcc67b0c8bda98d16e
ciazhar <daisetsunakama@gmail.com>
2017-04-06 18:24:18 +0700

    membuat controller bug dan list bug html

---
+++ src/main/java/com/ciazhar/controllers/BugController.java
@@ -0,0 +1,47 @@
+package com.ciazhar.controllers;
+
+import com.ciazhar.dao.BugDao;
+import com.ciazhar.entity.Bug;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * Created by ciazhar on 4/6/17.
+ */
+
+@Controller
+@RequestMapping("/api")
+public class BugController {
+
+    @Autowired
+    private BugDao dao;
+
+    @RequestMapping(value = "/bug", method = RequestMethod.GET)
+    @ResponseBody
+    public Page<Bug> daftarBug(Pageable page){
+        return dao.findAll(page);
+    }
+
+    @RequestMapping(value="/bug/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.OK)
+    public void hapusBug(@PathVariable("id") String id){
+        dao.delete(id);
+    }
+
+    @RequestMapping(value="/bug", method = RequestMethod.POST)
+    public void insertBugBaru(@RequestBody @Valid Bug b){
+        dao.save(b);
+    }
+
+    @RequestMapping(value="/bug/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Bug findBug(@PathVariable("id")String id){
+        return dao.findOne(id);
+    }
+}
